52468debdf84a06402852f453fe5bed2564e6fa1
Kalekki <kalekkiorg@gmail.com>
2022-11-14 21:16:12 +0200

    Regenerate, variant, upscale buttons

--- bot.py
+++ bot.py
@@ -11,11 +11,12 @@ from discord.ui import Button, View
 from PIL import Image, PngImagePlugin
 
 # TODO:
-# - Buttons for Regenerate, Variant, Upscale
 # - Restore faces
+# - Have the view be a class for reuse with img2img
+#       Remove the buttons after some time.
 # - Error handling
-# - Variation generation (NovelAI uses img2img with same prompt, seed+1, denoising_strength 0.8)
-# - Upscaling
+# - Clean this shit up
+#       Upscale is very hacky, ideally we'd want it to only affect the embed image while keeping the parameters
 
 
 config = configparser.ConfigParser()
@@ -47,25 +48,71 @@ async def text2img_command(interaction: discord.Interaction,
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     filename = text2img(prompt, negative_prompt, steps,
                         cfg_scale, width, height, seed, enable_hr=high_resolution_fix)
+    _filename = filename # needed for variant generation
     time_took = round((datetime.datetime.now(
     ) - datetime.datetime.strptime(timestamp, "%Y%m%d-%H%M%S")).total_seconds(), 2)
 
     # Create discord embed out of the image
-    embed = discord.Embed(title=prompt, color=0xffff00)
-    embed.set_image(url=f"attachment://{filename}.png")
-    img_info = read_png_info(f'images/{filename}.png')
-    if prompt != img_info['prompt']:
-        embed.add_field(name="Full prompt",
-                        value=f"{img_info['prompt']}", inline=False)
-    if img_info['negative_prompt'] != '':
-        embed.add_field(name="Negative prompt",
-                        value=f"{img_info['negative_prompt']}", inline=False)
-    embed.add_field(
-        name="Settings", 
-        value=f"Steps: `{img_info['steps']}`, Sampler: `{img_info['sampler']}`, Size: `{img_info['size']}`, \
-                CFG Scale: `{img_info['cfg_scale']}`, Seed: `{img_info['seed']}`",
-        inline=True)
-    embed.set_footer(text=f"Generation took: {time_took}s")
+    view = View()
+    view.timeout = 3000
+    embed = create_embed(filename,prompt,time_took)
+
+    # Regenerate
+    async def regenerate_callback(interaction: discord.Interaction):
+        # modify original message with new image
+        await interaction.response.defer()
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        text2img(prompt, negative_prompt, steps,
+                            cfg_scale, width, height, seed, enable_hr=high_resolution_fix)
+        time_took = round((datetime.datetime.now(
+        ) - datetime.datetime.strptime(timestamp, "%Y%m%d-%H%M%S")).total_seconds(), 2)
+        # Create discord embed out of the image
+        embed = create_embed(filename, prompt, time_took)
+        await interaction.message.edit(embed=embed, view=view, attachments=[discord.File(f'images/{filename}.png')])
+
+    async def variant_callback(interaction: discord.Interaction):
+        await interaction.response.defer()
+        # Get generated image and its seed
+        img = Image.open(f'images/{_filename}.png')
+        img_info = read_png_info(f'images/{_filename}.png')
+        seed = int(img_info['seed'])
+        negative_prompt = img_info['negative_prompt']
+
+        #Measure how long it took to generate the image
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        filename = img2img(img, prompt,negative_prompt=negative_prompt, denoising_strength=0.7, seed=int(seed)+1)
+        time_took = round((datetime.datetime.now(
+        ) - datetime.datetime.strptime(timestamp, "%Y%m%d-%H%M%S")).total_seconds(), 2)
+
+        # Create discord embed out of the image
+        embed = create_embed(filename, prompt, time_took)
+        await interaction.message.edit(embed=embed, view=view, attachments=[discord.File(f'images/{filename}.png')])
+
+    async def upscale_callback(interaction: discord.Interaction):
+        await interaction.response.defer()
+        # Get generated image and its seed
+        img = Image.open(f'images/{_filename}.png')
+
+        #Measure how long it took to generate the image
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        upscaled_file = upscale(img, prompt)
+        time_took = round((datetime.datetime.now(
+        ) - datetime.datetime.strptime(timestamp, "%Y%m%d-%H%M%S")).total_seconds(), 2)
+
+        # Follow up with the upscaled image
+        await interaction.followup.send("2x Upscaled image",ephemeral=True,files=[discord.File(f'images/{upscaled_file}.png')])
+
+
+    # Buttons for Regenerate, Variant, Upscale with callbacks
+    regenerate_button = Button(label="Regenerate", style=discord.ButtonStyle.blurple)
+    variant_button = Button(label="Variant", style=discord.ButtonStyle.blurple)
+    upscale_button = Button(label="Upscale", style=discord.ButtonStyle.blurple)
+    regenerate_button.callback = regenerate_callback
+    variant_button.callback = variant_callback
+    upscale_button.callback = upscale_callback
+    view.add_item(regenerate_button)
+    view.add_item(variant_button)
+    view.add_item(upscale_button)
 
     await interaction.edit_original_response(content=f"{interaction.user.mention} Your image is ready!", attachments=[discord.File(f"images/{filename}.png")], embed=embed, view=view)
 
@@ -74,6 +121,7 @@ async def text2img_command(interaction: discord.Interaction,
 async def image2image_command(interaction: discord.Interaction,
                               prompt: str,
                               image: discord.Attachment,
+                              negative_prompt: Optional[str],
                               denoising_strength: Optional[app_commands.Range[float, 0.1, 1.0]],
                               cfg_scale: Optional[app_commands.Range[float, 0.1, 30.0]],
                               steps: Optional[app_commands.Range[int, 10, 60]],
@@ -82,26 +130,12 @@ async def image2image_command(interaction: discord.Interaction,
     await interaction.response.send_message("Generating...")
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     img = Image.open(io.BytesIO(await image.read()))
-    filename = img2img(img, prompt, denoising_strength, cfg_scale, steps, seed)
+    filename = img2img(img, prompt,negative_prompt, denoising_strength, cfg_scale, steps, seed)
     time_took = round((datetime.datetime.now(
     ) - datetime.datetime.strptime(timestamp, "%Y%m%d-%H%M%S")).total_seconds(), 2)
 
     # Create discord embed out of the image
-    embed = discord.Embed(title=prompt, color=0xffff00)
-    embed.set_image(url=f"attachment://{filename}.png")
-    img_info = read_png_info(f'images/{filename}.png')
-    if prompt != img_info['prompt']:
-        embed.add_field(name="Full prompt",
-                        value=f"{img_info['prompt']}", inline=False)
-    if img_info['negative_prompt'] != '':
-        embed.add_field(name="Negative prompt",
-                        value=f"{img_info['negative_prompt']}", inline=False)
-    embed.add_field(
-        name="Settings", 
-        value=f"Steps: `{img_info['steps']}`, Sampler: `{img_info['sampler']}`, Size: `{img_info['size']}`, \
-                CFG Scale: `{img_info['cfg_scale']}`, Seed: `{img_info['seed']}`, Denoising Strength: `{img_info['denoising_strength']}`",
-        inline=True)
-    embed.set_footer(text=f"Generation took: {time_took}s")
+    embed = create_embed(filename, prompt, time_took)
 
     await interaction.edit_original_response(content=f"{interaction.user.mention} Your image is ready!", attachments=[discord.File(f"images/{filename}.png")], embed=embed)
 
@@ -113,35 +147,47 @@ async def on_ready():
     print(client.user.name)
     # Register commands manually, uncomment if you add/change commands or parameters
     try:
-       await tree.sync()
+        await tree.sync()
     except Exception as e:
-       print(f'Failed to sync bot commands with Discord, {e}')
+        print(f'Failed to sync bot commands with Discord, {e}')
 
 
 def response_to_image(response, prompt):
-    for i in response['images']:
-        image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
+    #if response has 'images' key, from txt2img or img2img, else its upscaled
+    if 'images' in response:
+        for i in response['images']:
+            image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
+            if len(prompt) > 40:
+                prompt = prompt[:40]
+            filename = ''.join(e for e in prompt if e.isalnum())
+            if filename == '':
+                filename = 'image'
+            png_payload = {
+                "image": "data:image/png;base64," + i
+            }
+            response2 = requests.post(
+                f'http://{sdsettings["address"]}/sdapi/v1/png-info', json=png_payload)
+            png_info = PngImagePlugin.PngInfo()
+            png_info.add_text('parameters', response2.json().get("info"))
+            image.save(f'images/{filename}.png', pnginfo=png_info)
+            print(f'images/{filename}.png saved')
+            # save the response for debugging
+            response.pop('images', None)
+            response.pop('init_images', None)
+            with open('response.json', 'w') as f:
+                f.write(str(response))
+                f.close()
+            return filename
+    else:
+        image = Image.open(io.BytesIO(base64.b64decode(response['image'])))
         if len(prompt) > 40:
             prompt = prompt[:40]
         filename = ''.join(e for e in prompt if e.isalnum())
         if filename == '':
             filename = 'image'
-        png_payload = {
-            "image": "data:image/png;base64," + i
-        }
-        response2 = requests.post(
-            f'http://{sdsettings["address"]}/sdapi/v1/png-info', json=png_payload)
-        png_info = PngImagePlugin.PngInfo()
-        png_info.add_text('parameters', response2.json().get("info"))
-        image.save(f'images/{filename}.png', pnginfo=png_info)
-        print(f'images/{filename}.png saved')
-        # save the response for debugging
-        response.pop('images', None)
-        response.pop('init_images', None)
-        with open('response.json', 'w') as f:
-            f.write(str(response))
-            f.close()
-        return filename
+        image.save(f'images/{filename}x2.png')
+        return filename+'x2'
+
 
 
 def text2img(prompt, negative_prompt=None, steps=None, cfg_scale=None, width=None, height=None, seed=None, enable_hr=None):
@@ -175,17 +221,18 @@ def text2img(prompt, negative_prompt=None, steps=None, cfg_scale=None, width=Non
     filename = response_to_image(response.json(), prompt)
     if enable_hr:
         img = Image.open(f'images/{filename}.png')
-        filename = img2img(img, prompt, 0.7, cfg_scale, steps, seed)
+        filename = img2img(img, prompt,negative_prompt, 0.7, cfg_scale, steps, seed)
     return filename
 
 
-def img2img(img, prompt, denoising_strength=None, cfg_scale=None, steps=None, seed=None):
+def img2img(img, prompt, negative_prompt=None, denoising_strength=None, cfg_scale=None, steps=None, seed=None):
     # Set default values if not provided
     denoising_strength = float(
         sdsettings['denoising_strength']) if denoising_strength is None else denoising_strength
     cfg_scale = float(sdsettings['cfg_scale']
                       ) if cfg_scale is None else cfg_scale
     steps = int(sdsettings['steps']) if steps is None else steps
+    negative_prompt = sdsettings['negative_prompt'] if negative_prompt is None else negative_prompt
     seed = -1 if seed is None else seed
 
     # Resize if needed
@@ -210,6 +257,7 @@ def img2img(img, prompt, denoising_strength=None, cfg_scale=None, steps=None, se
         "width": width,
         "height": height,
         "prompt": prompt,
+        "negative_prompt": negative_prompt,
         "steps": steps,
         "cfg_scale": cfg_scale,
         "denoising_strength": denoising_strength,
@@ -220,6 +268,22 @@ def img2img(img, prompt, denoising_strength=None, cfg_scale=None, steps=None, se
     response = requests.post(url, json=req)
     filename = response_to_image(response.json(), prompt)
     return filename
+
+
+def upscale(img, prompt):
+    url = f'http://{sdsettings["address"]}/sdapi/v1/extra-single-image'
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    img_base64 = base64.b64encode(img_byte_arr).decode('utf-8')
+    req = {
+        "image": "data:image/png;base64," + img_base64,
+        "upscaling_resize": 2,
+        "upscaler_1": sdsettings['upscaler'],
+    }
+    response = requests.post(url, json=req)
+    upscaled_file = response_to_image(response.json(), prompt)
+    return upscaled_file
 
 
 def getSamplers():
@@ -235,27 +299,54 @@ def read_png_info(filename):
     with open(filename, 'rb') as f:
         png = PngImagePlugin.PngImageFile(f)
         info = png.info.get('parameters')
-        _info = info.split('\n')
-        prompt = _info[0]
-        if len(_info) > 2:
-            negative_prompt = _info[1].split(': ')[1]
+        # if there is info, which is not the case with upscaled images
+        if info:
+            _info = info.split('\n')
+            prompt = _info[0]
+            if len(_info) > 2:
+                negative_prompt = _info[1].split(': ')[1]
+            else:
+                negative_prompt = ''
+            settings = _info[len(_info)-1].split(', ')
+            info_obj = {
+                'prompt': prompt,
+                'negative_prompt': negative_prompt,
+                'steps': int(settings[0].split(': ')[1]),
+                'sampler': settings[1].split(': ')[1],
+                'cfg_scale': float(settings[2].split(': ')[1]),
+                'seed': int(settings[3].split(': ')[1]),
+                'size': settings[4].split(': ')[1],
+                'model_hash': settings[5].split(': ')[1],
+                'seed_resize_from': settings[6].split(': ')[1],
+                'denoising_strength': float(settings[7].split(': ')[1]),
+                'ensd': int(settings[8].split(': ')[1]),
+            }
+            return info_obj
         else:
-            negative_prompt = ''
-        settings = _info[len(_info)-1].split(', ')
-        info_obj = {
-            'prompt': prompt,
-            'negative_prompt': negative_prompt,
-            'steps': int(settings[0].split(': ')[1]),
-            'sampler': settings[1].split(': ')[1],
-            'cfg_scale': float(settings[2].split(': ')[1]),
-            'seed': int(settings[3].split(': ')[1]),
-            'size': settings[4].split(': ')[1],
-            'model_hash': settings[5].split(': ')[1],
-            'seed_resize_from': settings[6].split(': ')[1],
-            'denoising_strength': float(settings[7].split(': ')[1]),
-            'ensd': int(settings[8].split(': ')[1]),
-        }
-        return info_obj
+            info_obj = {
+                'prompt': 'Upscaled image 2x',
+                'negative_prompt': '',
+            }
+            return info_obj
+
+def create_embed(filename, prompt, time_took):
+# Create discord embed out of the image
+    embed = discord.Embed(title=prompt, color=0xffff00)
+    embed.set_image(url=f"attachment://{filename}.png")
+    img_info = read_png_info(f'images/{filename}.png')
+    if prompt != img_info['prompt']:
+        embed.add_field(name="Full prompt",
+                        value=f"{img_info['prompt']}", inline=False)
+    if img_info['negative_prompt'] != '':
+        embed.add_field(name="Negative prompt",
+                        value=f"{img_info['negative_prompt']}", inline=False)
+    embed.add_field(
+        name="Settings", 
+        value=f"Steps: `{img_info['steps']}`, Sampler: `{img_info['sampler']}`, Size: `{img_info['size']}`, \
+                CFG Scale: `{img_info['cfg_scale']}`, Seed: `{img_info['seed']}`, Denoising Strength: `{img_info['denoising_strength']}`",
+        inline=True)
+    embed.set_footer(text=f"Generation took: {time_took}s")
+    return embed
 
 
 client.run(token)
